34d9e4e6c20779e8095c6de7711f5e9982335e5e
sbdmendoza <mendozastephen186@gmail.com>
2026-02-19 15:04:02 -0500

    set up basic app function

--- Code/test.py
+++ Code/test.py
@@ -1,1 +1,26 @@
 import numpy as np
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLabel
+
+# setting up application
+class AppTest(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('MultiModal Scanner (Name WIP)')
+
+        self.button = QPushButton('click')
+        self.resize(500, 500)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+# setting up window to open
+app = QApplication(sys.argv)
+window = AppTest()
+window.show()
+
+sys.exit(app.exec())
