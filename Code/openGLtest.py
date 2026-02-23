22d7fc952848d374f23e6aa5f45a5187f3880962
Jonathan Pearson <89711242+Snailydude@users.noreply.github.com>
2026-02-23 15:49:01 -0500

    added pyopengl to dependencies

--- Code/openGLtest.py
+++ Code/openGLtest.py
@@ -1,30 +1,28 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QDialog
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from OpenGL.GL import *
 import os
 
-# Import the generated UI
+# Import the UI
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "UI"))
 from ui_OpenGLTest import Ui_Dialog
 
 class OpenGLRenderer(QOpenGLWidget):
     def initializeGL(self):
         """Initialize OpenGL settings"""
-        gl = self.context().functions()
-        gl.glClearColor(0.1, 0.1, 0.1, 1.0)
-        gl.glEnable(gl.GL_DEPTH_TEST)
+        glClearColor(0.1, 0.1, 0.1, 1.0)
+        glEnable(GL_DEPTH_TEST)
     
     def resizeGL(self, w, h):
         """Handle window resize"""
-        gl = self.context().functions()
-        gl.glViewport(0, 0, w, h)
+        glViewport(0, 0, w, h)
     
     def paintGL(self):
         """Render the scene"""
-        gl = self.context().functions()
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-class AppTest(QMainWindow):
+class AppTest(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
