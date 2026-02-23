d408117b55c771af070d9b43170fde09caa0545a
Jonathan Pearson <89711242+Snailydude@users.noreply.github.com>
2026-02-23 16:05:21 -0500

    updates to opengltest

--- Code/openGLtest.py
+++ Code/openGLtest.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from PySide6.QtWidgets import QApplication, QDialog
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from OpenGL.GL import *
@@ -13,14 +14,83 @@ class OpenGLRenderer(QOpenGLWidget):
         """Initialize OpenGL settings"""
         glClearColor(0.1, 0.1, 0.1, 1.0)
         glEnable(GL_DEPTH_TEST)
+        
+        # Set up projection matrix
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        aspect = self.width() / self.height() if self.height() > 0 else 1.0
+        gluPerspective(45.0, aspect, 0.1, 100.0)
+        
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
     
     def resizeGL(self, w, h):
         """Handle window resize"""
         glViewport(0, 0, w, h)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        aspect = w / h if h > 0 else 1.0
+        gluPerspective(45.0, aspect, 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
     
     def paintGL(self):
         """Render the scene"""
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glTranslatef(0.0, 0.0, -3.0)
+        
+        # Draw a cube
+        self.draw_cube()
+    
+    def draw_cube(self):
+        """Draw a simple cube"""
+        glBegin(GL_QUADS)
+        
+        # Front face (white)
+        glColor3f(1.0, 1.0, 1.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        
+        # Back face (red)
+        glColor3f(1.0, 0.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        glVertex3f( 1.0, -1.0, -1.0)
+        
+        # Top face (green)
+        glColor3f(0.0, 1.0, 0.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        
+        # Bottom face (blue)
+        glColor3f(0.0, 0.0, 1.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f( 1.0, -1.0, -1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        
+        # Right face (yellow)
+        glColor3f(1.0, 1.0, 0.0)
+        glVertex3f( 1.0, -1.0, -1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        
+        # Left face (cyan)
+        glColor3f(0.0, 1.0, 1.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        
+        glEnd()
 
 class AppTest(QDialog):
     def __init__(self):
