0b18349ecc877e6f7cb186579b608cdd037fb538
Jonathan Pearson <89711242+Snailydude@users.noreply.github.com>
2026-03-02 15:39:57 -0500

    file import code

--- .vscode/Test Code/fileimport.py
+++ .vscode/Test Code/fileimport.py
@@ -10,7 +10,6 @@ class ImportReconstruction:
     downsampling = None
     data = None
 
-
     # For a given tiff stack, we need to know the folder, how much downsampling, 
     def __init__(self, folder_path, downsampling):
         self.folder_path = folder_path
@@ -24,6 +23,7 @@ class ImportReconstruction:
         # Filter for tiff images
         image_files = [f for f in file_list if f.lower().endswith(('.tiff'))]
 
+        # Tiff image dimensions 2938 x 2938 pixels @ 3661 x 3661 DPI    
         # Make a tiff stack from the images.
         # DOWNSAMPLE
 
