6a598d8ff4ca3671eafa73ef49e2157ad5f4636a
sbdmendoza <mendozastephen186@gmail.com>
2026-03-02 22:54:03 -0500

    Fixed instructions for edits to UI at the bottom of test.py

--- Code/test.py
+++ Code/test.py
@@ -13,10 +13,6 @@ if __name__ == "__main__":
 
 # Important Note: Every time a change is made to UI_V1.ui, make sure to rerun the following line in the terminal:
 #
-# pyside6-uic Code/UI_V1.ui -o ui_mainwindow.py
+# pyside6-uic Code/UI_V1.ui -o Code/ui_mainwindow.py
 #
-# This is assuming you are working in the directory folder for Multi_Modal_Spring2026. Then afterward, since the 
-# file will be made in that directory, move the newly created file into the Code folder so it is in the same
-# directory as the main Python file that will run this code.
-# Addendum: When replacing the ui_mainwindow.py file, for some reason it adds 'Code.' to the import at the beginning
-# of this file. Make sure to fix that back to the way it was before (without Code. at the beginning) for this to run.
+# By adding the folder at the beginning, this puts the newly created Python file in the right folder.
